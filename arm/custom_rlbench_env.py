fb5dbf64f9e25b38293e9eadeea6b34fce460e69
mandizhao <mandi.zhao@berkeley.edu>
2021-08-01 15:20:57 -0700

    align with original main

--- arm/custom_rlbench_env.py
+++ arm/custom_rlbench_env.py
@@ -1,6 +1,5 @@
-from typing import Type, List
-
 import numpy as np
+from typing import Type, List
 from pyrep.const import RenderMode
 from pyrep.errors import IKError, ConfigurationPathError
 from pyrep.objects import VisionSensor, Dummy
@@ -8,7 +7,6 @@ from rlbench import ObservationConfig, CameraConfig
 from rlbench.action_modes import ActionMode
 from rlbench.backend.observation import Observation
 from rlbench.backend.task import Task
-from rlbench.backend.utils import task_file_to_task_class
 from rlbench.task_environment import InvalidActionError
 
 from yarr.agents.agent import ActResult, VideoSummary
@@ -16,11 +14,12 @@ from yarr.envs.rlbench_env import RLBenchEnv
 from yarr.utils.observation_type import ObservationElement
 from yarr.utils.transition import Transition
 
+# New(Mandi):
+from rlbench.backend.utils import task_file_to_task_class 
+from hydra.utils import instantiate
 from collections import OrderedDict
 from copy import deepcopy
-from hydra.utils import instantiate
 import random 
-
 RECORD_EVERY = 20
 
 
@@ -34,9 +33,7 @@ class CustomRLBenchEnv(RLBenchEnv):
                  dataset_root: str = '',
                  channels_last: bool = False,
                  reward_scale=100.0,
-                 headless: bool = True,
-                 state_includes_remaining_time: bool = True,
-                 include_previous_action: bool = False):
+                 headless: bool = True,):
         super(CustomRLBenchEnv, self).__init__(
             task_class, observation_config, action_mode, dataset_root,
             channels_last, headless=headless)
@@ -47,53 +44,16 @@ class CustomRLBenchEnv(RLBenchEnv):
         self._previous_obs, self._previous_obs_dict = None, None
         self._recorded_images = []
         self._episode_length = episode_length
-        self._state_includes_remaining_time = state_includes_remaining_time
-        self._include_previous_action = include_previous_action
         self._i = 0
-        self._prev_action = None
 
     @property
-    def observation_elements(self): # -> List[ObservationElement]:
-        elements = []
-        low_dim_state_len = 0
-        if self._observation_config.joint_positions:
-            low_dim_state_len += 7
-        if self._observation_config.joint_forces:
-            low_dim_state_len += 7
-        if self._observation_config.gripper_open:
-            low_dim_state_len += 1
-        if self._observation_config.gripper_joint_positions:
-            low_dim_state_len += 2
-        if self._observation_config.gripper_touch_forces:
-            low_dim_state_len += 6
-        if self._observation_config.task_low_dim_state:
-            raise NotImplementedError()
-
-        if self._state_includes_remaining_time:
-            low_dim_state_len += 1
-
-        if self._include_previous_action:
-            low_dim_state_len += 8
-
-        if low_dim_state_len > 0:
-            elements.append(ObservationElement(
-                'low_dim_state', (low_dim_state_len, ), np.float32))
-        elements.extend(self._get_cam_observation_elements(
-            self._observation_config.left_shoulder_camera, 'left_shoulder'))
-        elements.extend(self._get_cam_observation_elements(
-            self._observation_config.right_shoulder_camera, 'right_shoulder'))
-        elements.extend(self._get_cam_observation_elements(
-            self._observation_config.front_camera, 'front'))
-        elements.extend(self._get_cam_observation_elements(
-            self._observation_config.wrist_camera, 'wrist'))
-        elements.extend(self._get_cam_observation_elements(
-            self._observation_config.overhead_camera, 'overhead'))
-        self.low_dim_state_len = low_dim_state_len
-
-        return elements
-
-    def set_task(self, task_class: Type[Task]):
-        self._task = self._rlbench_env.get_task(task_class)
+    def observation_elements(self) -> List[ObservationElement]:
+        obs_elems = super(CustomRLBenchEnv, self).observation_elements
+        for oe in obs_elems:
+            if oe.name == 'low_dim_state':
+                oe.shape = (oe.shape[0] - 7 * 2,)  # remove pose and joint velocities as they will not be included
+                self.low_dim_state_len = oe.shape[0]
+        return obs_elems
 
     def extract_obs(self, obs: Observation, t=None, prev_action=None):
         obs.joint_velocities = None
@@ -102,64 +62,14 @@ class CustomRLBenchEnv(RLBenchEnv):
         obs.gripper_pose = None
         obs.gripper_matrix = None
         obs.wrist_camera_matrix = None
-
-        # Turn gripper quaternion to be positive w
-        if obs.gripper_pose is not None and obs.gripper_pose[-1] < 0:
-            obs.gripper_pose[3:] *= -1.0
-
         if obs.gripper_joint_positions is not None:
             obs.gripper_joint_positions = np.clip(
                 obs.gripper_joint_positions, 0., 0.04)
 
         obs_dict = super(CustomRLBenchEnv, self).extract_obs(obs)
-
-        if self._state_includes_remaining_time:
-            tt = 1. - ((self._i if t is None else t) / self._episode_length)
-            obs_dict['low_dim_state'] = np.concatenate([obs_dict['low_dim_state'], [tt]]).astype(np.float32)
-
-        if self._include_previous_action:
-            pa = self._prev_action if prev_action is None else prev_action
-            pa = np.zeros((8,)) if pa is None else pa
-            obs_dict['low_dim_state'] = np.concatenate([obs_dict['low_dim_state'], pa]).astype(np.float32)
-
         obs.gripper_matrix = grip_mat
         obs.gripper_pose = grip_pose
-        for (k, v) in [(k, v) for k, v in obs_dict.items() if 'point_cloud' in k]:
-            obs_dict[k] = v.astype(np.float32)
-
-        for config, name in [
-            (self._observation_config.left_shoulder_camera, 'left_shoulder'),
-            (self._observation_config.right_shoulder_camera, 'right_shoulder'),
-            (self._observation_config.front_camera, 'front'),
-            (self._observation_config.wrist_camera, 'wrist'),
-            (self._observation_config.overhead_camera, 'overhead')]:
-            if config.point_cloud:
-                obs_dict['%s_camera_extrinsics' % name] = obs.misc['%s_camera_extrinsics' % name]
-                obs_dict['%s_camera_intrinsics' % name] = obs.misc['%s_camera_intrinsics' % name]
-
         return obs_dict
-
-    def _get_cam_observation_elements(self, camera: CameraConfig, prefix: str):
-        elements = []
-        img_s = list(camera.image_size)
-        shape = img_s + [3] if self._channels_last else [3] + img_s
-        if camera.rgb:
-            elements.append(
-                ObservationElement('%s_rgb' % prefix, shape, np.uint8))
-        if camera.point_cloud:
-            elements.append(
-                ObservationElement('%s_point_cloud' % prefix, shape, np.float32))
-            # elements.append(
-            #     ObservationElement('%s_pixel_coord' % prefix, (2,), np.int32))
-            elements.append(
-                ObservationElement('%s_camera_extrinsics' % prefix, (4, 4),
-                                   np.float32))
-            elements.append(
-                ObservationElement('%s_camera_intrinsics' % prefix, (3, 3),
-                                   np.float32))
-        if camera.mask:
-            raise NotImplementedError()
-        return elements
 
     def launch(self):
         super(CustomRLBenchEnv, self).launch()
@@ -173,16 +83,13 @@ class CustomRLBenchEnv(RLBenchEnv):
             self._record_cam.set_pose(cam_placeholder.get_pose())
             self._record_cam.set_render_mode(RenderMode.OPENGL)
 
-    def reset(self): # -> dict:
+    def reset(self) -> dict:
+        self._previous_obs_dict = super(CustomRLBenchEnv, self).reset()
         self._record_current_episode = (
                 self.eval and self._episode_index % RECORD_EVERY == 0)
         self._episode_index += 1
         self._recorded_images.clear()
         self._i = 0
-        self._prev_action = None
-        descriptions, obs = self._task.reset()
-        self._previous_obs = obs
-        self._previous_obs_dict = self.extract_obs(obs)
         return self._previous_obs_dict
 
     def register_callback(self, func):
@@ -203,14 +110,12 @@ class CustomRLBenchEnv(RLBenchEnv):
         final_frames[:, :, :, 1 if success else 0] = 255
         self._recorded_images.extend(list(final_frames))
 
-    def step(self, act_result: ActResult): #-> Transition:
+    def step(self, act_result: ActResult) -> Transition:
         action = act_result.action
         success = False
         obs = self._previous_obs_dict  # in case action fails.
         try:
             obs, reward, terminal = self._task.step(action)
-            if self._previous_obs is None:
-                self._previous_obs = obs
             if terminal:
                 success = True
                 reward *= self._reward_scale
@@ -224,7 +129,6 @@ class CustomRLBenchEnv(RLBenchEnv):
 
         summaries = []
         self._i += 1
-        self._prev_action = action
         if ((terminal or self._i == self._episode_length) and
                 self._record_current_episode):
             self._append_final_frame(success)
@@ -236,6 +140,7 @@ class CustomRLBenchEnv(RLBenchEnv):
         d, = self._task.get_demos(
             1, live_demos=False, random_selection=False, from_episode_number=i)
         self._task.reset_to_demo(d)
+
 
 class MultiTaskRLBenchEnv(CustomRLBenchEnv):
     def __init__(self,
@@ -320,46 +225,3 @@ class MultiTaskRLBenchEnv(CustomRLBenchEnv):
         self._previous_obs = obs
         self._previous_obs_dict = self.extract_obs(obs)
         return self._previous_obs_dict
-
-# class MultiTaskRLBenchEnv:
-#     """Stores a list of envs with different tasks"""
-#     def __init__(self, train_tasks, eval_tasks, obs_config, action_mode, single_env_cfg):
-#         self.env_list = OrderedDict()
-#         self.task_names = {'train':[], 'eval': []}
-#         for task in train_tasks:
-#             env_cfg = deepcopy(single_env_cfg)
-#             self.env_list[task] = CustomRLBenchEnv(
-#                 observation_config=obs_config,
-#                 action_mode=action_mode,
-#                 task_class=task_file_to_task_class(task),
-#                 **env_cfg)
-#             print(f"Created Env for task {task}")
-#             self.task_names['train'].append(task)
-#         for task in eval_tasks:
-#             self.task_names['eval'].append(task)
-#             if task not in self.env_list.keys():
-#                 print(f'Creating eval-only task: {task}')
-#                 env_cfg = deepcopy(single_env_cfg)
-#                 self.env_list[task] = CustomRLBenchEnv(
-#                     observation_config=obs_config,
-#                     action_mode=action_mode,
-#                     task_class=task_file_to_task_class(task),
-#                     **env_cfg)
-             
-#         self.n_train_tasks = len(self.task_names['train'])
-#         self.n_eval_tasks = len(self.task_names['eval'])
-#         self.n_unique_tasks = len( set(self.task_names['train'] + self.task_names['eval']) )
-        
-#         state_len = self.env_list[tasks[0]].low_dim_state_len
-#         for env in self.env_list.values():
-#             assert env.low_dim_state_len == state_len, 'Better if every env has the same state length '
-#         self.low_dim_state_len = state_len 
-
-
-#     def get_task_env(self, task):
-#         assert task in self.env_list.keys(), f'Task {task} does not exist!'
-#         return self.env_list[task]
-    
-#     def get_unique_tasks(self):
-#         """Omits the overlap tasks between train and eval"""
-#         return sorted( set(self.task_names['train'] + self.task_names['eval']) )
